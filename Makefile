7d9cca990c630ce6a0c4990ea0d2ffc84fee476e
Peter Becich <peterbecich@gmail.com>
2017-11-12 18:12:02 -0800

    bogus response for `tickerQuery` endpoint

--- Makefile
+++ Makefile
@@ -4,18 +4,18 @@ frontend:
 	npm install  && \
 	bower install && \
 	pulp build && \
-	pulp browserify --to static/Main.js
+	pulp browserify --to Main.js
 
 watch_frontend:
 	cd stock-frontend && \
 	npm install  && \
 	bower install && \
 	pulp build && \
-	pulp --watch browserify --to static/Main.js
+	pulp --watch browserify --to Main.js
 
 watch_frontend_prod:
 	cd stock-frontend && \
 	npm install  && \
 	bower install && \
 	pulp build && \
-	pulp --watch build -O --to static/Main.js
+	pulp --watch browserify -O --to Main.js
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,22 +1,27 @@
 {-# LANGUAGE DataKinds       #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeOperators   #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Lib
     ( startApp
     ) where
 
-import System.Random (randomRIO)
+import Control.Monad.IO.Class (liftIO)
+
+import GHC.Generics
 
 import Data.Aeson
 import Data.Aeson.TH
-
 import Data.Time.Clock
 
 import Network.Wai
 import Network.Wai.Handler.Warp
+
 import Servant
 
-import Control.Monad.IO.Class (liftIO)
+import System.Random (randomRIO)
 
 data User = User
   { userId        :: Int
@@ -26,11 +31,19 @@ data User = User
 
 $(deriveJSON defaultOptions ''User)
 
+data TickerQueryResponse = TickerQueryResponse
+  { tickerSymbol :: String
+  , description :: String
+  } deriving Generic
+
+instance ToJSON TickerQueryResponse
+
+fakeTickerQueryResponse = TickerQueryResponse "foo" "bar"
+
 type API = "time" :> Get '[JSON] UTCTime
+         :<|> "tickerQuery" :> QueryParam "q" String :> Get '[JSON] TickerQueryResponse
          :<|> "randomInt" :> Get '[PlainText] String
          :<|> "i" :> Raw
-
-type StaticAPI = "i" :> Raw
 
 startApp :: IO ()
 startApp = run 1234 app
@@ -42,15 +55,22 @@ api :: Proxy API
 api = Proxy
 
 server :: Server API
-server = (liftIO getCurrentTime)
-       :<|> (liftIO $ show <$> (randomRIO(1,10) :: IO Int))
-       :<|> (serveDirectoryWebApp "stock-frontend/static")
+server = timeEndpoint
+  :<|> tickerQueryEndpoint
+  :<|> randomIntEndpoint
+  :<|> staticEndpoint
+  where timeEndpoint :: Handler UTCTime
+        timeEndpoint = liftIO getCurrentTime
 
-users :: [User]
-users = [ User 1 "Isaac" "Newton"
-        , User 2 "Albert" "Einstein"
-        , User 3 "Stephen" "Hawking"
-        ]
+        tickerQueryEndpoint :: Maybe String -> Handler TickerQueryResponse
+        tickerQueryEndpoint (Just query) = pure fakeTickerQueryResponse
+        tickerQueryEndpoint Nothing = pure fakeTickerQueryResponse
+
+        randomIntEndpoint :: Handler String
+        randomIntEndpoint = liftIO $ show <$> (randomRIO(1,10) :: IO Int)
+
+        staticEndpoint :: Server Raw
+        staticEndpoint = serveDirectoryWebApp "stock-frontend"
 
 -- https://hackage.haskell.org/package/servant-server-0.11.0.1/docs/Servant-Server-Internal-Handler.html
 
