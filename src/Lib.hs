6d5ac2c870b57d4d689db4362f8e716ca842f515
Peter Becich <peterbecich@gmail.com>
2017-11-17 08:19:18 -0800

    timespan query param

--- src/Lib.hs
+++ src/Lib.hs
@@ -14,6 +14,7 @@ import GHC.Generics
 
 import Data.Aeson
 import Data.Aeson.TH
+import Data.Monoid
 import Data.Time.Clock
 
 import Network.Wai
@@ -35,7 +36,7 @@ $(deriveJSON defaultOptions ''User)
 
 type API = "time" :> Get '[JSON] UTCTime
          :<|> "tickerQuery" :> QueryParam "q" String :> Get '[JSON] TickerQueryResponse
-         :<|> "correlated" :> QueryParam "q" String :> QueryParam "limit" Int :> Get '[JSON] [TickerQueryResponse]
+         :<|> "correlated" :> QueryParam "q" String :> QueryParam "limit" Int :> QueryParam "timespan" Int :> Get '[JSON] [TickerQueryResponse]
          :<|> "randomInt" :> Get '[PlainText] String
          :<|> "i" :> Raw
 
@@ -58,13 +59,19 @@ server = timeEndpoint
         timeEndpoint = liftIO getCurrentTime
 
         tickerQueryEndpoint :: Maybe String -> Handler TickerQueryResponse
-        tickerQueryEndpoint (Just query) = pure fakeTickerQueryResponse
+        tickerQueryEndpoint (Just query) = do
+          liftIO $ putStrLn $ "query: " <> query
+          pure fakeTickerQueryResponse
         tickerQueryEndpoint Nothing = pure fakeTickerQueryResponse
 
-        correlatedEndpoint :: Maybe String -> Maybe Int -> Handler [TickerQueryResponse]
-        correlatedEndpoint (Just query) (Just limit) = pure $ take limit (fakeStocks query)
-        correlatedEndpoint (Just query) Nothing = pure $ take 10 (fakeStocks query)
-        correlatedEndpoint Nothing _ = pure $ []
+        correlatedEndpoint :: Maybe String -> Maybe Int -> Maybe Int -> Handler [TickerQueryResponse]
+        correlatedEndpoint (Just query) (Just limit) (Just timespan) =
+          pure $ take limit (fakeStocks query)
+        correlatedEndpoint (Just query) Nothing (Just timespan) =
+          pure $ take 10 (fakeStocks query)
+        correlatedEndpoint (Just query) (Just limit) Nothing =
+          pure $ take 10 (fakeStocks query)
+        correlatedEndpoint Nothing _ _ = pure $ []
 
         randomIntEndpoint :: Handler String
         randomIntEndpoint = liftIO $ show <$> (randomRIO(1,10) :: IO Int)
